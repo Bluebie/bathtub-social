d2c9ebf451b1924a31c83bdeddde865a26656738
Phoenix Fox <Bluebie@users.noreply.github.com>
2020-06-01 20:18:57 +1000

    crypto/identity upgraded with key from hashed data, and fetch request methods

--- library/crypto/identity.js
+++ library/crypto/identity.js
@@ -4,8 +4,13 @@ const nacl = require('tweetnacl')
 const uri = require('encodeuricomponent-tag')
 
 class Identity {
-  constructor() {
-    this.keyPair = nacl.sign.keyPair()
+  constructor(privateKey) {
+    if (typeof(privateKey) == 'string') {
+      let privateKeyBuffer = Buffer.from(privateKey, 'base64')
+      if (privateKeyBuffer.byteLength != nacl.sign.secretKeyLength) throw new Error('Private Key length is incorrect')
+    } else {
+      this.keyPair = nacl.sign.keyPair()
+    }
     this.base64 = {
       publicKey: Buffer.from(this.keyPair.publicKey).toString('base64')
     }
@@ -53,6 +58,38 @@ class Identity {
       }
     })
   }
+
+  get(path, options = {}) {
+    return this.signedFetch(path, {
+      method: 'GET',
+      ...options
+    })
+  }
+
+  async getJSON(path, options = {}) {
+    let response = await this.get(path, options)
+    return response.json()
+  }
+
+  post(path, options = {}) {
+    return this.signedFetch(path, {
+      method: 'POST',
+      ...options
+    })
+  }
+
+  async postJSON(path, object, options = {}) {
+    let response = await this.post(path, { body: JSON.stringify(object), headers: { 'Content-Type': 'application/json', ...(options.headers || {}) } })
+    return response.json()
+  }
+}
+
+// create an identity with a consistent secret key from a hash of arbitrary length data
+Identity.fromHashedData = async function(buffer) {
+  if (buffer.arrayBuffer) buffer = await buffer.arrayBuffer() // if it looks like a Blob, convert to arrayBuffer
+  if (!Buffer.isBuffer(buffer)) buffer = Buffer.from(buffer) // if it isn't a node-style buffer, convert it to one
+  if (buffer.byteLength < 64) console.warn("Identity's input is very small, resulting private key may not be secure")
+  return new Identity(nacl.hash(buffer)) // build a new identity, using a hash of the file's contents as the secret key
 }
 
 module.exports = Identity
