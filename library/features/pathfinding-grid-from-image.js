7a7f53905b7d0d01717fee054106fc3f73fdaf4f
Phoenix Fox <Bluebie@users.noreply.github.com>
2020-06-02 00:46:13 +1000

    pathfinding-grid-from-image bugfixes

--- library/features/pathfinding-grid-from-image.js
+++ library/features/pathfinding-grid-from-image.js
@@ -7,7 +7,7 @@ const loadScaledImageData = require('./load-scaled-image-data')
 // cameraAngle in degrees
 module.exports = async function pathfindingGridFromImage(imageUrl, width = 512, cameraAngle = 45) {
   // load the Image
-  let img = await loadImage(src)
+  let img = await loadImage(imageUrl)
   // calculate how tall this grid should be, from maintaining the image's ratio
   let topViewHeight = img.height / img.width * width
   // calculate how distorted the view is from a non-topdown camera angle
@@ -15,17 +15,18 @@ module.exports = async function pathfindingGridFromImage(imageUrl, width = 512, 
   // calculate an integer height for the grid that maintains a topdown sort of perspective in the grid scaling
   let height = Math.round(topViewHeight / heightScaling)
   // load image data scaled to that shape
-  let imageData = loadScaledImageData(imageUrl, width, height)
+  let imageData = loadScaledImageData(img, width, height)
   let threshold = 255 / 2
 
   let matrix = []
   for (let y = 0; y < imageData.height; y++) {
     let row = []
-    let yStart = x * (imageData.width * 4)
+    let yStart = y * (imageData.width * 4)
     for (let x = 0; x < imageData.width; x++) {
       let green = imageData.data[yStart + (x * 4) + 1]
       row.push(green > threshold ? 0 : 1)
     }
+    matrix.push(row)
   }
 
   let grid = new PF.Grid(matrix)
